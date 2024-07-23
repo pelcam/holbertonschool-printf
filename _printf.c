844629048d9495c7f362b5a36ba08781243f9aee
gdbmrm <>
2024-07-23 12:29:58 +0000

    modification printf

--- _printf.c
+++ _printf.c
@@ -2,28 +2,22 @@
 #include <unistd.h>
 
 /**
- */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
-/**
- */
 int _printstr(char *s)
 {
 	return(write(1, s, sizeof(s) - 1));
 }
 
-/**
- */
 caracter_t spe[] = {
     {"c", _putchar},
     {"s", _printstr},
     {NULL, NULL}
 };
 
-/**
  */
 int _printf(const char *format, ...)
 {
@@ -31,15 +25,6 @@ int _printf(const char *format, ...)
 
     for (i = 0; format[i] != '\0'; i++)
     {
-        if (format[i] == "%")
-        {
-            if (format[i + 1] == 'c')
-            {
-            }
-            if (format[i + 1] == 's')
-            {
-            }
-        }
         _putchar(format[i]);
     }
 }
