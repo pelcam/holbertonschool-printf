7b904532c685d66dc2e56ead5f815a4c1fa950c9
gdbmrm <>
2024-07-26 12:55:25 +0000

    change

--- test/_printf.c
+++ test/_printf.c
@@ -4,28 +4,34 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* _printf - fonction identique a la commande printf
+*
+* @format: chaine fixe a afficher
+*
+* Return: nombre de charactere copier
+*/
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0;
-	va_start(args, format);
 
+	va_start(args, format);
 	if (format == NULL) /* check si format est nul*/
 	{
 		return (-1);
 	}
 
-	for (i = 0; format[i] != '\0'; i++) /* parcours format*/
+	while (*format) /* parcours format*/
 	{
-		if (format[i] == '%') /* check si format est un mod*/
+		if (format[i] == '%' && format[i + 1] != '\0') /* check si format est un %*/
 		{
 			get_spe_func(format[i + 1])(args);
-			i++;
 		} else
 		{
 			printchar(format[i]);
 		}
+		format++;
 	}
 
 	va_end(args);
