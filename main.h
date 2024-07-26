81a43d8ee2730000361b919eef107f0a28a483d5
gdbmrm <>
2024-07-26 12:56:03 +0000

    change main.h

--- main.h
+++ main.h
@@ -5,11 +5,13 @@
 typedef struct caracter
 {
 	char *c;
-	int (*f)(char);
+	void (*f)(args);
 } caracter_t;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int _printstr(char *s);
-
+void _printnum(va_list args);
+void _printmod(va_list args __attribute__((unused)));
+void _putchar(va_list args);
+void _printstr(va_list args);
+void printchar(char c);
 #endif /* MAIN_H */
